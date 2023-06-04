0b0bcb7884b33920a90877dfda6a6cc4568df935
614uc0 <glaucomv@gmail.com>
2023-06-04 15:53:13 -0300

    Initial commit

---
+++ app/src/main/java/devandroid/glaucomv/applistacurso/controller/PessoaController.java
@@ -0,0 +1,24 @@
+package devandroid.glaucomv.applistacurso.controller;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.glaucomv.applistacurso.model.Pessoa;
+
+public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller", "Controller iniciada");
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller", "Salvo: "+pessoa.toString());
+
+    }
+}
---
+++ app/src/main/java/devandroid/glaucomv/applistacurso/view/MainActivity.java
@@ -0,0 +1,110 @@
+package devandroid.glaucomv.applistacurso.view;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.glaucomv.applistacurso.R;
+import devandroid.glaucomv.applistacurso.controller.PessoaController;
+import devandroid.glaucomv.applistacurso.model.Pessoa;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    SharedPreferences preferences;
+    public static final String NOME_PREFERENCES = "pref_listavip";
+
+    PessoaController controller;
+    Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    EditText editPrimeiroNome;
+    EditText editSobreNomeAluno;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(NOME_PREFERENCES,0);
+        SharedPreferences.Editor listavip = preferences.edit();
+
+        controller = new PessoaController();
+        controller.toString();
+
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome","NA"));
+        pessoa.setSobreNome(preferences.getString("sobreNome","NA"));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso","NA"));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato","NA"));
+
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobreNomeAluno = findViewById(R.id.editSobreNomeAluno);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNomeAluno.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editPrimeiroNome.setText("");
+                editSobreNomeAluno.setText("");
+                editTelefoneContato.setText("");
+                editNomeCurso.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Volte sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobreNomeAluno.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo " + pessoa.toString(), Toast.LENGTH_LONG).show();
+
+                listavip.putString("primeiroNome",pessoa.getPrimeiroNome());
+                listavip.putString("sobreNome",pessoa.getSobreNome());
+                listavip.putString("nomeCurso",pessoa.getCursoDesejado());
+                listavip.putString("telefoneContato",pessoa.getTelefoneContato());
+                listavip.apply();
+
+                controller.salvar(pessoa);
+
+            }
+        });
+
+        Log.i("POOAndroid", "Objeto pessoa: " + pessoa.toString());
+
+    }
+}
